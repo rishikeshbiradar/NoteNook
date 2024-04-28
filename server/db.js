8709b4c5b4ffb7b5e60ad4a742849f6c023d8bf2
rishikeshbiradar <rushisb.55@gmail.com>
2024-04-29 01:58:34 +0530

    Updated Database

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // const mongoURI = "mongodb+srv://sam:qE8bQPKGKPNPDQ7s@users.nvyog12.mongodb.net/todolist?retryWrites=true&w=majority"
-const mongoURI="mongodb://localhost:27017/DBMS"
+const mongoURI="mongodb://localhost:27017/NoteNook"
 
 const connectToMongo = () => {
   mongoose.connect(mongoURI, () => {
