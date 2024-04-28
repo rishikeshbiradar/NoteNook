de90e37c661f12c954f143de2024489b583c7e86
Rushikesh Biradar <115314561+rishikeshbiradar@users.noreply.github.com>
2024-04-29 01:59:23 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-// const mongoURI = "mongodb+srv://sam:qE8bQPKGKPNPDQ7s@users.nvyog12.mongodb.net/todolist?retryWrites=true&w=majority"
+// const mongoURI = "mongodb+srv://sam:qE8bQPKGKPNPDQ7s@users.nvyog12.mongodb.net/NoteNook?retryWrites=true&w=majority"
 const mongoURI="mongodb://localhost:27017/NoteNook"
 
 const connectToMongo = () => {
